f95a962af462f688bbd142a65f104924c3783e57
dholness2 <DHolness@Dons-MacBook-Pro.local>
2015-08-09 17:56:20 -0400

    ai minimax updates

--- lib/ai_minimax.rb
+++ lib/ai_minimax.rb
@@ -1,38 +1,75 @@
-class Computer
 
-  def initialize (maximizing_player,minimizing_player )
+require_relative "board"
+
+class Computer
+  attr_accessor :choice
+
+  def  initialize (maximizing_player,minimizing_player )
     @maximizing_player = maximizing_player
     @minimizing_player = minimizing_player
-  end
-
-  def possible_board_states(board,maximizing_player)
-
+    @choice
 
   end
 
-  def score (player)
-    return 10  if win(max_player)
-    return -10 if win(min_player)
-    no_win = o
+  def avaliable_moves(board)
+     moves= []
+     board.board.flatten.each_with_index{|x,i| moves.push i if x =="_"}
+     moves
   end
 
+  def score (depth,board)
+    return 10 - depth  if board.winner == @maximizing_player
+    return -10 - depth if board.winner == @minimizing_player
+    draw = 0
+  end
 
   def minimax (board,depth, maximizing_player)
-    return move if board.winner
-    depth+=1
+     scores = []
+     moves = []
+    return score(depth,board) if board.winner
 
     if maximizing_player
-      best_move = []
-      for possible_board_states.each do | board|
-          value = minimax(board,depth -1,false)
-          best_value = best_move.max
-        end
-        else
-          best_move= []
-          for possible_board_states.each |board|
-            val = minimax(board,depth-1,TRUE)
-            best_move=[] = min (bestValue,val)
+      avaliable_moves(board).each do |current_move|
+          possible_board_state = Board.new(:board_size=>3)
+          possible_board_state.board = Marshal::load(Marshal.dump(board.board))
+          possible_board_state.move(@maximizing_player,current_move)
+          scores.push minimax(possible_board_state,depth+1,false)
+          moves.push current_move
           end
-        end
+
+        max_score_index = scores.each_with_index.max[1]
+        @choice = moves[max_score_index]
+        return scores[max_score_index]
+    else
+      avaliable_moves(board).each do |current_move|
+        possible_board_state = Board.new(:board_size=>3)
+        possible_board_state.board = Marshal::load(Marshal.dump(board.board))
+      possible_board_state.move(@maximizing_player,current_move)
+        scores.push minimax(possible_board_state,depth+1,false)
+          moves.push current_move
+          end
+        min_score_index = scores.each_with_index.min[1]
+        @choice = moves[min_score_index]
+        return scores[min_score_index]
       end
-    end
+  end
+end
+
+test_board = Board.new(:board_size=> 3)
+
+test_board.move("x",0)
+test_board.move("y",1)
+test_board.move("x",5)
+test_board.move("y",2)
+print test_board
+ p test_board.winner
+
+ y ="y"
+ x = "x"
+ test_ai = Computer.new y, x
+ p test_ai.minimax(test_board,4,true)
+ p test_ai.choice
+ p test_board.winner
+#  p test_board.board.transpose
+
+
--- lib/board.rb
+++ lib/board.rb
@@ -4,8 +4,7 @@ class Board
   def initialize (args)
     @board_size = args[:board_size]
     @board = Array.new((@board_size)){Array.new(@board_size ,"_")}
-    @move_count = 0
-  end
+    end
 
   def to_s
     board.map{|row| row.join("|")+"\n"}.join("")
@@ -18,7 +17,7 @@ class Board
     y =  cordinates[0]
     return nil if !(board[y][x] == "_")
     board[y][x] = player
-    move_count += 1
+    board
   end
 
   def draw?
@@ -28,7 +27,7 @@ class Board
   def row_check (matrix)
     winner = nil
     matrix.each do |row|
-      winner = row[0] if row.uniq.size == 1 && row[0] !="_"
+      winner = row[0] if row.uniq.size == 1 && !(row.include?"_")
     end
     winner
   end
@@ -37,11 +36,24 @@ class Board
     row_check(board.transpose)
   end
 
+  def diagnoal_check()
+    winner = nil
+    d_one= []
+    d_two =[]
+    board_size.times{|i| d_one.push board[i][i] }
+    board_size.times{|i| d_two.push board.reverse[i][i] }
+    winner = d_one[0] if d_one.uniq.size == 1 && d_one[0] !="_"
+    winner = d_two[0] if d_two.uniq.size == 1 && d_two[0] !="_"
+    winner
+  end
+
   def winner
     if  row_check(board)
       return row_check(board)
     elsif row_check(board.transpose)
       return row_check(board.transpose)
+    elsif diagnoal_check()
+      return diagnoal_check()
     elsif draw?
       return "draw"
     else
@@ -51,15 +63,20 @@ class Board
 end
 # test_board = Board.new(:board_size=> 3)
 
-# test_board.move("x",0)
-# test_board.move("y",1)
-# test_board.move("x",2)
-# test_board.move("y",3)
-# test_board.move("x",4)
-# test_board.move("y",5)
-# test_board.move("y",6)
-# test_board.move("x",7)
-# test_board.move("_",8)
 
-# puts test_board
+# test_board.move("y",2)
+# test_board.move("y",4)
+# test_board.move("y",8)
+# test_board.move("x",2)
+# test_board.move("x",6)
+# test_board.move("y",7)
+
+
+# print test_board
+
+# p b = test_board.current_state
+# p test_board.board.object_id
+#  b.move("y",1)
+# print test_board
 # print test_board.winner
+
