050c8beb21b16dcffcf666a0927302d74ab0b9e4
dholness2 <DHolness@Dons-MacBook-Pro.local>
2015-08-17 21:00:21 -0400

    terminal updated for input/output functions

--- lib/Terminal_view.rb
+++ lib/Terminal_view.rb
@@ -1,13 +1,15 @@
 class TerminalView
-
-  def initialize
+   attr_accessor :input, :output
+  def initialize()
+    @input  = $stdin
+    @output = $stdout
   end
 
   def welcome
     puts "DON's TIC TAC TOE\n"
   end
   def key
-    puts "Move key:\n0|1|2\n3|4|5\n6|7|8\n"
+    puts "Move key:\n1|2|3\n4|5|6\n7|8|9\n"
     puts " "
   end
   def clear
@@ -22,6 +24,15 @@ class TerminalView
     gets.downcase
   end
   def get_move
-    gets.to_i
+   "Select your move : \n"
+    move = input.gets.chomp
+    return move.to_i if move =~/^[1-9]$/
+    output.puts "Invalid move"
   end
+
+  def board board
+   puts "#{board[0]}#{board[1]}#{board[2]}"
+   puts "#{board[3]}#{board[4]}#{board[5]}"
+   puts "#{board[6]}#{board[7]}#{board[7]}"
+ end
 end
