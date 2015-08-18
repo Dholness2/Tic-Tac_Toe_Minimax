b01874bff2a755eb440174e63ac62bf582ae3a98
dholness2 <DHolness@Dons-MacBook-Pro.local>
2015-08-17 21:03:09 -0400

    updates

--- lib/tic_tac_toe_controller.rb
+++ lib/tic_tac_toe_controller.rb
@@ -1,17 +1,28 @@
-class TicTacController
+class TicTacToeController
   attr_reader :tic_tac_toe,:whatson,:view
   def initialize (args)
     @tic_tac_toe = args[:board]
     @whatson = args[:computer]
     @view = args[:view]
   end
+
+def get_users_move
+    view.display"Select your move : \n"
+    user_move =  view.get_move
+    until tic_tac_toe.move("x",user_move)
+      view.display "invalid move, Select again?"
+      user_move = view.get_move
+    end
+  end
+
+
   def run
 
     until tic_tac_toe.winner
       view.clear
       view.welcome
       view.key
-      view.display tic_tac_toe
+      view.board tic_tac_toe.board
       view.display"Select your move : \n"
       user_move =  view.get_move
       until tic_tac_toe.move("x",user_move)
@@ -20,7 +31,7 @@ class TicTacController
       end
       view.clear
       view.key
-      view.display tic_tac_toe
+      view.board tic_tac_toe.board
       break if tic_tac_toe.winner
       whatson.minimax(tic_tac_toe,tic_tac_toe.move_count,true)
       until tic_tac_toe.move("o",whatson.move) || tic_tac_toe.winner
@@ -28,9 +39,9 @@ class TicTacController
         ai_move = whatson.move
       end
       view.clear
-      view.display tic_tac_toe
-       view.display "Result : #{tic_tac_toe.winner} "+ "\n"
-     end
+      view.board tic_tac_toe.board
+      view.display "Result : #{tic_tac_toe.winner} "+ "\n"
+    end
 
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,6 @@
 require_relative'../lib/board'
 require_relative'../lib/computer'
+require_relative'../lib/terminal_view'
+require_relative'../lib/tic_tac_toe_controller'
+require 'stringio'
+
