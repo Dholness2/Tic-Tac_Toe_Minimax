4a1c3b99b1a96c86be259d3a98769c47a01e480a
dholness2 <DHolness@Dons-MacBook-Pro.local>
2015-08-17 20:55:36 -0400

    benchmark array data structure vs hash test

--- lib/benchmarks/computer_benchmarks.rb
+++ lib/benchmarks/computer_benchmarks.rb
@@ -1,5 +1,6 @@
-require_relative 'board'
-require 'benchmark/ips'
+# require'../board'
+# require'../BoardTwo'
+# require 'benchmark/ips'
 
 class Computer
   attr_accessor :move
@@ -21,36 +22,36 @@ class Computer
     draw = 0
   end
 
-  def minimax (board,depth, maximizing_player)
-    return score(depth,board) if board.winner
-    scores = []
-    moves = []
-    if maximizing_player
-      avaliable_moves(board).each do |possible_move|
-        board.move(@maximizing_player,possible_move)
-        scores.push minimax(board,depth+1,false)
-        moves.push possible_move
-        board.undo_move(possible_move)
-      end
+  # def minimax (board,depth, maximizing_player)
+  #   return score(depth,board) if board.winner
+  #   scores = []
+  #   moves = []
+  #   if maximizing_player
+  #     avaliable_moves(board).each do |possible_move|
+  #       board.move(@maximizing_player,possible_move)
+  #       scores.push minimax(board,depth+1,false)
+  #       moves.push possible_move
+  #       board.undo_move(possible_move)
+  #     end
 
-      max_score_index = scores.each_with_index.max[1]
-      @move = moves[max_score_index]
-      return scores[max_score_index]
-    else
-      avaliable_moves(board).each do |possible_move|
-        board.move(@minimizing_player,possible_move)
-        scores.push minimax(board,depth+1,true)
-        moves.push possible_move
-        board.undo_move(possible_move)
-      end
-      min_score_index = scores.each_with_index.min[1]
-      @move = moves[min_score_index]
-      return scores[min_score_index]
-    end
-  end
+  #     max_score_index = scores.each_with_index.max[1]
+  #     @move = moves[max_score_index]
+  #     return scores[max_score_index]
+  #   else
+  #     avaliable_moves(board).each do |possible_move|
+  #       board.move(@minimizing_player,possible_move)
+  #       scores.push minimax(board,depth+1,true)
+  #       moves.push possible_move
+  #       board.undo_move(possible_move)
+  #     end
+  #     min_score_index = scores.each_with_index.min[1]
+  #     @move = moves[min_score_index]
+  #     return scores[min_score_index]
+  #   end
+  # end
 
 
-def minimax_two (board,depth, maximizing_player)
+def minimax (board,depth, maximizing_player)
     return score(depth,board) if board.winner
     scores = {}
 
@@ -62,31 +63,32 @@ def minimax_two (board,depth, maximizing_player)
       end
 
       max_move = scores.max_by {|k,v| v}
-      @move = max_move[0]
-      return max_move[1]
+      @move = max_move[0].to_i
+    return max_move[1]
     else
       board.available_moves.each do |possible_move|
         board.move(@minimizing_player,possible_move)
-        scores["#{possible_move}"] = minimax(board,depth+1,false)
+        scores["#{possible_move}"] = minimax(board,depth+1,true)
         board.undo_move(possible_move)
       end
       min_move = scores.min_by {|k,v| v}
-      @move = min_move[0]
+      @move = min_move[0].to_i
       return min_move[1]
     end
   end
 end
 
- test_board = Board.new(:board_size=> 3,:empty_position_placeholder=>"_")
-    [0].each{|move| test_board.move("o",move)}
-computer_marker ="o"
-user_marker ="x"
-whatson = Computer.new(:max_player => computer_marker,:mini_player => user_marker)
+#  test_board = Board.new(:board_size=> 3,:empty_position_placeholder=>"_")
+#  test_board_two = BoardTwo.new(:board_size=> 3,:empty_position_placeholder=>"_")
+#     [0].each{|move| test_board.move("o",move)}
+# computer_marker ="o"
+# user_marker ="x"
+# whatson = Computer.new(:max_player => computer_marker,:mini_player => user_marker)
 
-  Benchmark.ips do |x|
-    x.report('available_moves_slow'){  whatson.minimax(test_board,1,"x")}
-    x.report('avaliable_moves_fast') { whatson.minimax_two(test_board,1,"x") }
-    x.compare
-  end
+#   Benchmark.ips do |x|
+#     x.report('available_moves_slow'){  whatson.minimax(test_board,1,"x")}
+#     x.report('avaliable_moves_fast') { whatson.minimax_two(test_board_two,1,"x") }
+#     x.compare
+#   end
 
 
