7169df1f12e63505bc92ffe11d36c13ad5c4e6af
dholness2 <DHolness@Dons-MacBook-Pro.local>
2015-08-31 11:31:13 -0400

    update the controller to reflect dependence abstraction changes
    -change controller object names from computer/player to player_one to pLayer_two
    -add test for the run function
    -add test for the get player name function
    -ad test for the game initialize function

--- spec/tic_tac_toe_controller_spec.rb
+++ spec/tic_tac_toe_controller_spec.rb
@@ -1,38 +1,84 @@
 require 'spec_helper'
 describe TicTacToeController do
-  let(:test_player) {HumanPlayer.new(:marker=> "x")}
-  let(:test_board) { Board.new(:board_size=> 3,:empty_position_placeholder=>"_") }
-  let(:test_computer) {Computer.new(:max_marker => "o",:mini_marker => "x")}
   let(:test_view) { TerminalView.new()}
-  let(:test_controller) { TicTacToeController.new(:board_size=> 3,:empty_position_placeholder=>"_",:view =>test_view,:board => test_board,:computer=> test_computer,:player=> test_player)}
+  let(:test_player) {HumanPlayer.new(:marker=> "x",:view=>test_view)}
+  let(:test_board) { Board.new(:board_size=> 3,:empty_position_placeholder=>"_") }
+  let(:test_computer) {Computer.new(:max_marker => "o",:mini_marker => "x",:name=>"whatson")}
+  let(:test_controller) { TicTacToeController.new(:board_size=> 3,:empty_position_placeholder=>"_",:view =>test_view,:board => test_board,:player_two=> test_computer,:player_one=> test_player)}
 
-  it "should update the board based on paramaters" do
-    test_controller.update_board(test_player.marker,0)
-    expect(test_board.board[0][0]).to eq(test_player.marker)
-  end
 
-  it "should get the player name and update players object name" do
+  it "should ask the players name if they player oject does not have a name" do
     test_view.input = StringIO.new("donovan")
     test_view.output = StringIO.new()
-    test_controller.get_player_name
+    test_controller.get_player_name(test_controller.player_one,"1")
+    expect(test_controller.player_one.name).to eq("donovan")
+  end
+
+  it "should get the players name and update players object name" do
+    test_view.input = StringIO.new("donovan")
+    test_view.output = StringIO.new()
+    test_controller.game_init
     expect(test_player.name).to eq("donovan")
   end
 
-  it "should  take the player next move and update the board " do
-    test_view.input = StringIO.new("1")
+  it "should display the game state" do
     test_view.output = StringIO.new()
-    test_controller.get_player_move
-    expect(test_board.board[0][0]).to eq(test_player.marker)
+    test_controller.get_home_view
+    expect(test_view.output.string).to eq(
+      "\e[H\e[2J\n"+
+      "DON's TIC TAC TOE\n"+
+      "Move key:\n"+
+      "1|2|3\n"+
+      "4|5|6\n"+
+      "7|8|9\n"+
+      " \n_|_|_\n"+
+      "_|_|_\n"+
+    "_|_|_\n")
   end
 
-  it "should take the computer next move and update the board" do
-    test_board.move(test_player.marker,0)
-    test_controller.get_computer_move
-    expect(test_board.board[1][1]).to eq(test_computer.maximizing_marker)
-  end
-
-  it "should return true incase of a win"do
+  it "should return true incase of a win" do
     [0,3,6].each{|move| test_board.move(test_player.marker,move)}
     expect(test_controller.win).to  be_truthy
+  end
+
+  it " should run a game of tic tac toe" do
+    test_view.output =StringIO.new()
+    test_view.input =StringIO.new("donovan\n1\n2\n9\n")
+    test_controller.run
+    expect(test_view.output.string).to eq(
+      "\e[H\e[2J\nDON's TIC TAC TOE\n"+
+      "Move key:\n1|2|3\n4|5|6\n7|8|9\n \n_|_|_\n_|_|_\n_|_|_\n"+
+      "what is your name player1?"+
+      "\n\e[H\e[2J\n"+
+      "DON's TIC TAC TOE\nMove key:\n1|2|3\n4|5|6\n7|8|9\n"+
+      " \n_|_|_\n_|_|_\n_|_|_\n"+
+      "select your next move donovan"+
+      "\n\e[H\e[2J\n"+
+      "DON's TIC TAC TOE\nMove key:\n1|2|3\n4|5|6\n7|8|9\n"+
+      " \nx|_|_\n_|_|_\n_|_|_"+
+      "\n\e[H\e[2J\n"+
+      "DON's TIC TAC TOE\nMove key:\n1|2|3\n4|5|6\n7|8|9\n"+
+      " \nx|_|_\n_|o|_\n_|_|_"+
+      "\n\e[H\e[2J\n"+
+      "DON's TIC TAC TOE\nMove key:\n1|2|3\n4|5|6\n7|8|9\n"+
+      " \nx|_|_\n_|o|_\n_|_|_\n"+
+      "select your next move donovan"+
+      "\n\e[H\e[2J\n"+
+      "DON's TIC TAC TOE\nMove key:\n1|2|3\n4|5|6\n7|8|9\n"+
+      " \nx|x|_\n_|o|_\n_|_|_\n"+
+      "\e[H\e[2J\n"+
+      "DON's TIC TAC TOE\nMove key:\n1|2|3\n4|5|6\n7|8|9\n"+
+      " \nx|x|o\n_|o|_\n_|_|_\n"+
+      "\e[H\e[2J\n"+
+      "DON's TIC TAC TOE\nMove key:\n1|2|3\n4|5|6\n7|8|9\n"+
+      " \nx|x|o\n_|o|_\n_|_|_\n"+
+      "select your next move donovan"+
+      "\n\e[H\e[2J\n"+
+      "DON's TIC TAC TOE\nMove key:\n1|2|3\n4|5|6\n7|8|9\n"+
+      " \nx|x|o\n_|o|_\n_|_|x\n"+
+      "\e[H\e[2J\n"+
+      "DON's TIC TAC TOE\nMove key:\n1|2|3\n4|5|6\n7|8|9\n"+
+      " \nx|x|o\n_|o|_\no|_|x\n"+
+    "Result : o \n")
   end
 end
