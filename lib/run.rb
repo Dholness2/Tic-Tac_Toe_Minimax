4767134d1b73e48d3fee018074c5a2e119c2b95c
dholness2 <DHolness@Dons-MBP.home>
2015-08-31 12:37:22 -0400

    update the run file based on class updates
    -change the controller player arguements to player_two/one
    -change the computer objects arugments to take in watson string name

--- lib/run.rb
+++ lib/run.rb
@@ -3,11 +3,10 @@ require 'board'
 require 'computer'
 require 'terminal_view'
 require 'tic_tac_toe_controller'
-require 'player'
-
-human_player = Player.new(:marker=>"x")
-whatson = Computer.new(:max_marker =>"o",:mini_marker => human_player.marker)
-tic_tac_toe = Board.new(:board_size => 3, :empty_position_placeholder=>"_")
+require 'human_player'
 view = TerminalView.new()
-game = TicTacToeController.new(:board=> tic_tac_toe,:computer => whatson,:view=>view,:player=>human_player)
+human_player = HumanPlayer.new(:marker=>"x",:view=> view)
+watson = Computer.new(:max_marker =>"o",:mini_marker => human_player.marker,name:=>"watson")
+tic_tac_toe = Board.new(:board_size => 3, :empty_position_placeholder=>"_")
+game = TicTacToeController.new(:board=> tic_tac_toe,:player_two => whatson,:view=>view,:player_one=>human_player)
 game.run
