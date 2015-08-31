0ebb17e431d1fa7e034d52d0ba039555ff06b509
dholness2 <DHolness@Dons-MBP.home>
2015-08-31 13:02:55 -0400

    fixed object names in controller and computer args

--- lib/run.rb
+++ lib/run.rb
@@ -6,7 +6,7 @@ require 'tic_tac_toe_controller'
 require 'human_player'
 view = TerminalView.new()
 human_player = HumanPlayer.new(:marker=>"x",:view=> view)
-watson = Computer.new(:max_marker =>"o",:mini_marker => human_player.marker,name:=>"watson")
+watson = Computer.new(:max_marker =>"o",:mini_marker => human_player.marker,:name=>"watson")
 tic_tac_toe = Board.new(:board_size => 3, :empty_position_placeholder=>"_")
-game = TicTacToeController.new(:board=> tic_tac_toe,:player_two => whatson,:view=>view,:player_one=>human_player)
+game = TicTacToeController.new(:board=> tic_tac_toe,:player_two =>watson,:view=>view,:player_one=>human_player)
 game.run
