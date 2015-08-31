e26c4d76a54dca6dd9d481ddeca38cfef871bfeb
dholness2 <DHolness@Dons-MBP.home>
2015-08-31 12:40:45 -0400

    update human player class to manage user moves
    -add a get_move function
    -add a validate move function
    
    These changes allow for the ttt_controller to accept multiple player types.
    Human player concerns is to manage all human interactions within the game.

--- lib/human_player.rb
+++ lib/human_player.rb
@@ -1,9 +1,11 @@
 class HumanPlayer
-  attr_reader :move,:marker,:name
+  attr_reader :marker,:name
+  attr_accessor :current_move,:view
   def initialize(args={})
     @name = args[:name]
     @marker = args[:marker]
-    @move = args[:move]
+    @current_move = args[:move]
+    @view = args[:view]
   end
 
   def set_name input
@@ -14,7 +16,19 @@ class HumanPlayer
     @marker = input
   end
 
-  def update_move input
-    @move = input
+  def validated_move (move,board)
+    return nil unless move.is_a? Integer
+    return nil if move < 0 || move > ((board.board_size*board.board_size)-1)
+    true
+  end
+
+  def get_move (board)
+    view.display "select your next move #{name}"
+    current_move  = view.get_move
+    until validated_move(current_move,board)
+      view.display "invalid move, Select again?"
+      current_move = view.get_move
+    end
+    board.move(marker,current_move)
   end
 end
