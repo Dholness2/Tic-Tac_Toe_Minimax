33ac2757546b2e97085f20c385373927e4a6dd4f
dholness2 <DHolness@Dons-MBP.home>
2015-08-31 12:48:13 -0400

    add get_move functions and set_name function
    In order to decouple the ttt_controler form player times a get move function was abstracted to the computer class. It's the computers concern to get the next move based on the board state.

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,12 +1,14 @@
 class Computer
-  attr_accessor :move, :maximizing_marker, :minimizing_marker
+  attr_reader :name
+  attr_accessor :move, :maximizing_marker,:minimizing_marker,:current_move
   def initialize (args ={})
     @maximizing_marker = args[:max_marker]
     @minimizing_marker = args[:mini_marker]
-    @move
+    @current_move
+    @name = args[:name]
   end
 
-def score (depth,board)
+  def score (depth,board)
     return 10 - depth  if board.winner == @maximizing_marker
     return depth - 10  if board.winner == @minimizing_marker
     draw = 0
@@ -15,11 +17,11 @@ def score (depth,board)
   def best_move (scores,moves,maximizing)
     if maximizing
       max_score_index = scores.each_with_index.max[1]
-      @move = moves[max_score_index]
+      @current_move = moves[max_score_index]
       return scores[max_score_index]
     else
       min_score_index = scores.each_with_index.min[1]
-      @move = moves[min_score_index]
+      @current_move = moves[min_score_index]
       return scores[min_score_index]
     end
   end
@@ -37,5 +39,14 @@ def score (depth,board)
       board.undo_move(possible_move)
     end
     best_move(scores,moves,maximizing)
+  end
+
+  def get_move(board)
+    minimax(board,board.move_count,true)
+    board.move(maximizing_marker,current_move)
+  end
+
+  def set_name(input)
+    @name = input
   end
 end
